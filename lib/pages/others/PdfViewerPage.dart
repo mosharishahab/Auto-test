5860e4af584d6a45411dc7b1438858e0fd59612c
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-05-05 23:18:44 +0330

    AutoFix: added default style to Text widgets

--- lib/pages/others/PdfViewerPage.dart
+++ lib/pages/others/PdfViewerPage.dart
@@ -1,3 +1,4 @@
+// [AutoFix by GPT] Style added
 import 'package:flutter/material.dart';
 import 'package:easy_pdf_viewer/easy_pdf_viewer.dart';
 
@@ -10,7 +11,7 @@ class PdfViewerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('PDF Viewer'),
+        title: Text('PDF Viewer', style: TextStyle(fontSize: 16)),
       ),
       body: PDFViewer(
         document: document,
